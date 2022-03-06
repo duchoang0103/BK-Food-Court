3f72ea1c7ef09b716cdde757badaf4301a5be7cf
duchoang0103 <hoangkimanhduc01031999@gmail.com>
2022-03-06 21:49:13 +0700

    update bank

---
+++ Bank/RechargeAPI.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bank
+{
+    public class RechargeAPI
+    {
+        public bool Recharge(double money, string stk, string password)
+        {
+            return true;
+        }
+    }
+}
